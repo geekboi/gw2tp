58c54cccb69fb79a1b9dbd22746047251eca3c47
donovan <donovan>
2012-09-21 11:59:29 -0700

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gw2tp::Application.config.secret_token = '4f7ae174ab5008db425922b7d883cc8c4fb05dd76831512dfd43d64894c9f553e1a6920dd81c5c28b638134523f9b3d5f655b84ed82f0aa5b9b333b4cdc7fc8b'
